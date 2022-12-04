8db59a8697438a211258713203ff10414b273dd8
Nieky Allen <nieky.allen@gmail.com>
2022-12-04 03:43:01 -0600

    try mac env for actions

--- tests/e2e/drop-text-init.spec.ts
+++ tests/e2e/drop-text-init.spec.ts
@@ -1,36 +1,21 @@
-// import { test, expect } from '@playwright/test';
-// import { baseURL } from './config';
-// import { createPageForBrowser } from './util';
-
-// test('should start the drop session successfully', async ({ playwright }) => {
-//     const page = await createPageForBrowser(playwright.webkit);
-
-//     await page.goto(baseURL);
-
-//     const secretValue = 'super secret value';
-
-//     await page.click('text=Begin');
-
-//     await expect(page.getByRole('heading', { name: 'waiting for secrets' })).toBeVisible({
-//         timeout: 10_000,
-//     });
-
-//     await page.getByPlaceholder('Your secret').fill(secretValue);
-
-//     await page.click('text=Confirm Payload');
-// });
-
-
-
 import { test, expect } from '@playwright/test';
 import { baseURL } from './config';
+import { createPageForBrowser } from './util';
 
-test('should start the drop session successfully', async ({ page }) => {
+test('should start the drop session successfully', async ({ playwright }) => {
+    const page = await createPageForBrowser(playwright.webkit);
+
     await page.goto(baseURL);
+
+    const secretValue = 'super secret value';
 
     await page.click('text=Begin');
 
-    await expect(
-        page.getByRole('heading', { name: 'waiting for secrets' }),
-    ).toBeVisible({ timeout: 10_000 });
+    await expect(page.getByRole('heading', { name: 'waiting for secrets' })).toBeVisible({
+        timeout: 10_000,
+    });
+
+    await page.getByPlaceholder('Your secret').fill(secretValue);
+
+    await page.click('text=Confirm Payload');
 });
