1e090b9a79387411014a75affd0ea5aa3b9f1e45
Nieky Allen <nieky.allen@gmail.com>
2023-01-19 20:00:19 -0600

    progress on cli scripts

--- cli/index.ts
+++ cli/index.ts
@@ -1,39 +1,57 @@
 #!/usr/bin/env node
-
+import 'dotenv/config';
 import { Command } from 'commander';
-import { version } from './package.json';
+import { description, version } from './package.json';
 import { nanoid } from 'nanoid';
-import chalk from 'chalk';
-import figlet from 'figlet';
 import inquirer from 'inquirer';
+import { displayWelcomeMessage, logError } from 'lib/log';
+import { initPeer } from 'lib/peer';
+
 const program = new Command();
 
-program.name('deaddrop-cli').description('description').version(version);
+program.name('deaddrop-cli').description(description).version(version);
 
 program
     .command('drop')
     .argument('[input]', 'secret to drop')
     .option('-i, --input [input]', 'secret to drop')
     .option('-t, --type [dropType]', 'type of secret being dropped')
-    .action((input: string | undefined, options) => {
-        console.log(
-            chalk.green(
-                figlet.textSync('deaddrop', {
-                    font: 'Standard',
-                    horizontalLayout: 'default',
-                    verticalLayout: 'default',
-                }),
-            ),
-        );
-        if (input) console.log('input arg provided: ', input);
-        else console.log('input option provided: ', options.input);
+    .action(async (input: string | undefined, options) => {
+        displayWelcomeMessage();
+
+        const secretInput = input || options.input as string | undefined;
+
+        if (!secretInput) {
+            logError('Invalid input provided');
+            return;
+        }
+
+        const peer = await initPeer(nanoid());
+console.log(peer);
+        const answer = await inquirer.prompt([
+            {
+                name: 'testname',
+                type: 'input',
+                message: 'input here: ',
+            },
+        ]);
+
+        console.log(answer);
+        peer.disconnect();
     });
 
 program
     .command('grab')
     .argument('<id>', 'drop session ID')
-    .action((id: string) => {
+    .action(async (id: string) => {
         console.log('requesting drop with ID: ', id);
     });
 
 program.parse();
+
+const exitSignals: NodeJS.Signals[] = ['SIGINT', 'SIGTERM', 'SIGQUIT'];
+
+for (const signal in exitSignals)
+    process.on(signal, async (code) => {
+        console.log('PROGRAM EXITING');
+    });
