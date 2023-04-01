064c436c69822b66764b57ec077a0df91d9761f1
Nieky Allen <nieky.allen@gmail.com>
2023-04-01 01:39:49 -0500

    update grab section

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -14,17 +14,40 @@ import {
 import { useRouter } from 'next/router';
 import { GRAB_PATH } from '@config/paths';
 import { Faq } from 'molecules/Faq';
+import { useMediaQuery } from '@mantine/hooks';
 
 const Home = () => {
     const router = useRouter();
     const theme = useMantineTheme();
+    const isMobile = useMediaQuery(`(max-width: ${theme.breakpoints.sm}px)`);
     const inputRef = useRef<HTMLInputElement>();
+
+    const submitGrab = () => {
+        const inputVal = inputRef.current!.value;
+
+        const isUrl = inputVal.includes(
+            window.location.protocol + window.location.host,
+        );
+
+        const params = isUrl
+            ? inputVal.split('?')[1]
+            : new URLSearchParams({
+                  drop: inputRef.current!.value,
+              }).toString();
+
+        router.push(`${GRAB_PATH}?${params}`);
+    };
+
     return (
         <>
             <HeroBanner />
             <FeaturesSection />
             <Center
-                style={{ minHeight: '220px', paddingTop: theme.spacing.md }}
+                style={{
+                    minHeight: '230px',
+                    paddingTop: theme.spacing.xl,
+                    paddingBottom: theme.spacing.md,
+                }}
             >
                 <Card
                     id={'start-grab-section'}
@@ -32,35 +55,39 @@ const Home = () => {
                     shadow={'sm'}
                     style={{
                         width: '90%',
-                        maxWidth: '650px',
+                        maxWidth: '500px',
                         padding: theme.spacing.xl,
                     }}
                 >
                     <Title size={'h2'}>Grab a Secret</Title>
-                    <Text size={'sm'}>
+                    <Text size={'sm'} style={{ paddingTop: 2 }}>
                         Enter your drop ID (or link) that was shared with you!
                     </Text>
-                    <Group position={'left'}>
+                    <Group style={{ paddingTop: theme.spacing.md }}>
                         <TextInput
                             ref={inputRef as any}
-                            width={'100%'}
+                            styles={{
+                                root: isMobile
+                                    ? {
+                                          width: '100%',
+                                      }
+                                    : undefined,
+                            }}
+                            size={'md'}
                             variant={'filled'}
                             placeholder={'abcdef12345'}
                         />
                         <Button
-                            onClick={() => {
-                                const params = new URLSearchParams({
-                                    drop: inputRef.current!.value,
-                                });
-                                router.push(`${GRAB_PATH}?${params}`);
-                            }}
+                            size={'md'}
+                            fullWidth={isMobile}
+                            onClick={submitGrab}
                         >
                             Start
                         </Button>
                     </Group>
                 </Card>
             </Center>
-            <Faq/>
+            <Faq />
         </>
     );
 };
