f221d95d4d19023938a954c3fa4e11dafdb4c444
Nieky Allen <nieky.allen@gmail.com>
2023-08-01 00:50:04 -0500

    update stripe utils, impl confirmation page, update premium section

---
+++ web/pages/confirmation.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Center, Container, Title, useMantineTheme } from '@mantine/core';
+import { GetServerSideProps } from 'next/types';
+import { validateCheckoutSession } from 'api/stripe';
+
+const PremiumConfirmation = (props: { success: boolean }) => {
+    const theme = useMantineTheme();
+    return (
+        <Container style={{ maxWidth: '700px' }}>
+            <Center
+                style={{
+                    minHeight: '230px',
+                    paddingTop: theme.spacing.xl,
+                    paddingBottom: theme.spacing.md,
+                }}
+            >
+                <Title size={'h2'}>
+                    Premium{' '}
+                    {props.success ? 'Activated' : 'Subscription Failed'}!
+                </Title>
+            </Center>
+        </Container>
+    );
+};
+
+export const getServerSideProps: GetServerSideProps = async ({
+    query,
+    res,
+}) => {
+    res.setHeader('Cache-Control', 'private, maxage=600');
+
+    let success = false;
+    const checkoutSessionId = query.id;
+
+    if (checkoutSessionId && typeof checkoutSessionId === 'string') {
+        const sessionPaid = await validateCheckoutSession(checkoutSessionId);
+
+        if (sessionPaid) success = true;
+    }
+
+    return { props: { success } };
+};
+
+export default PremiumConfirmation;
--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -92,15 +92,37 @@ const Home = () => {
             <Faq />
             <Center
                 style={{
-                    minHeight: '230px',
+                    minHeight: '200px',
                     paddingTop: theme.spacing.xl,
                     paddingBottom: theme.spacing.md,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'space-evenly',
+                    alignItems: 'center',
                 }}
             >
-                <Button
-                    onClick={() => (window.location.href = paymentLink)}
-                    type={'button'}
-                >Get Premium</Button>
+                <Title size={'h4'}>
+                    Get unlimited daily drops and upcoming features by support
+                    deadrop!
+                </Title>
+                <Center
+                    style={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                    }}
+                >
+                    <Button
+                        onClick={() => (window.location.href = paymentLink)}
+                        type={'button'}
+                    >
+                        Get Premium
+                    </Button>
+                    <Text size={'xs'}>
+                        All licenses are attached to your email.
+                    </Text>
+                </Center>
             </Center>
         </>
     );
