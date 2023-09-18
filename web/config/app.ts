437d56ec71d112679f00cf62ed25b2a5537aa064
Nieky Allen <nieky.allen@gmail.com>
2023-09-18 23:46:43 +0200

    creat app config & static assets for PWA icons/configs

---
+++ web/config/app.ts
@@ -0,0 +1,8 @@
+export const title = 'deadrop';
+
+export const description = 'e2e enncrypted secret sharing';
+
+export const themeColors = {
+    primary: '#1971c2',
+    secondary: '#1a1b1e',
+};
