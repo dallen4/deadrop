56710ed8da9ebb7f68278a48de72ce158fc2601d
Nieky Allen <nieky.allen@gmail.com>
2023-10-01 23:42:12 -0500

    start action scaffolding for drop & grab

--- cli/actions/grab.ts
+++ cli/actions/grab.ts
@@ -1,3 +1,48 @@
+import { generateKeyPair } from '@shared/lib/crypto/operations';
+import { get } from '@shared/lib/fetch';
+import { initGrabContext } from '@shared/lib/machines/grab';
+import { DropDetails } from '@shared/types/common';
+import { loader } from 'lib/loader';
+import { displayWelcomeMessage, logError, logInfo } from 'lib/log';
+import { initPeer } from 'lib/peer';
+import { DROP_API_PATH } from '@shared/config/paths';
+
 export const grab = async (id: string) => {
-    console.log('requesting drop with ID: ', id);
+    const ctx = initGrabContext();
+    let currState = null;
+
+    ctx.id = id;
+
+    displayWelcomeMessage();
+
+    ctx.keyPair = await generateKeyPair();
+
+    logInfo('Key pair generated...');
+
+    loader.start('Initializing peer...');
+
+    ctx.peer = await initPeer();
+
+    loader.stop();
+
+    logInfo('Peer successfully connected!');
+
+    logInfo('Fetching drop details...');
+
+    const details = await get<DropDetails>(
+        process.env.DEADDROP_API_URL! + DROP_API_PATH,
+        {
+            id: ctx.id,
+        },
+    );
+
+    if (!details) {
+        logError(`Drop instance ${ctx.id} not found, closing connection...`);
+
+        // cleanup();
+        return;
+    }
+
+    ctx.dropperId = details.peerId;
+    ctx.nonce = details.nonce;
 };
