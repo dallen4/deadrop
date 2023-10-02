117da9aa735d6e692a5aed4143f3fdcd67766951
Nieky Allen <nieky.allen@gmail.com>
2023-10-01 23:37:05 -0500

    update peer init implementation

--- cli/lib/peer.ts
+++ cli/lib/peer.ts
@@ -1,8 +1,8 @@
 import type Peer from 'peerjs';
 import { createPeer } from '@shared/lib/peer';
 
-export const initPeer = (id: string) =>
-    createPeer(id, process.env.PEER_SERVER_URL!);
+export const initPeer = () =>
+    createPeer(process.env.PEER_SERVER_URL!);
 
 export const cleanupPeer = (peer: Peer) => {
     peer.disconnect();
