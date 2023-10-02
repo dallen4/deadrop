dddfbd6b856494565693140bec95b6dab32ef9cc
Nieky Allen <nieky.allen@gmail.com>
2023-10-01 23:18:02 -0500

    fix webrtc on apple silicon issue

--- cli/scripts/inject.js
+++ cli/scripts/inject.js
@@ -3,8 +3,8 @@ global.navigator = {
     platform: 'system',
 };
 
-global.RTCPeerConnection = require('wrtc').RTCPeerConnection;
-global.RTCSessionDescription = require('wrtc').RTCSessionDescription;
-global.RTCIceCandidate = require('wrtc').RTCIceCandidate;
+global.RTCPeerConnection = require('@koush/wrtc').RTCPeerConnection;
+global.RTCSessionDescription = require('@koush/wrtc').RTCSessionDescription;
+global.RTCIceCandidate = require('@koush/wrtc').RTCIceCandidate;
 
 global.WebSocket = require('ws');
