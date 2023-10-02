90feaab489fd914c2257f4aa16f4c0164d740b67
Nieky Allen <nieky.allen@gmail.com>
2023-10-01 23:42:12 -0500

    start action scaffolding for drop & grab

--- cli/actions/drop.ts
+++ cli/actions/drop.ts
@@ -1,9 +1,15 @@
-import type { PayloadInputMode } from '@shared/types/common';
+import type { InitDropResult, PayloadInputMode } from '@shared/types/common';
 import inquirer from 'inquirer';
 import { loader } from 'lib/loader';
 import { displayWelcomeMessage, logError, logInfo } from 'lib/log';
-import { initPeer } from 'lib/peer';
-import { nanoid } from 'nanoid';
+import { cleanupPeer, initPeer } from 'lib/peer';
+import { DataConnection } from 'peerjs';
+import { dropMachine, initDropContext } from '@shared/lib/machines/drop';
+import { DropEventType, DropState } from '@shared/lib/constants';
+import { generateKeyPair } from '@shared/lib/crypto/operations';
+import { post } from '@shared/lib/fetch';
+import { InitDropEvent } from '@shared/types/drop';
+import { DROP_API_PATH } from '@shared/config/paths';
 
 type DropOptions = {
     input?: string;
@@ -11,31 +17,77 @@ type DropOptions = {
 };
 
 export const drop = async (input: string | undefined, options: DropOptions) => {
+    const ctx = initDropContext();
+    let currState = null;
+
+    ctx.message = input || options.input || null;
+
     displayWelcomeMessage();
 
-    const secretInput = input || options.input;
+    if (!ctx.message) {
+        logInfo('No input provided...');
 
-    if (!secretInput) {
-        logError('Invalid input provided');
-        return;
+        const answer = await inquirer.prompt([
+            {
+                name: 'input',
+                type: 'input',
+                message: 'input here: ',
+            },
+        ]);
+
+        ctx.message = answer.input;
     }
+
+    ctx.keyPair = await generateKeyPair();
+
+    logInfo('Key pair generated...');
 
     loader.start('Initializing peer...');
 
-    const peer = await initPeer(nanoid());
+    ctx.peer = await initPeer();
 
     loader.stop();
 
     logInfo('Peer successfully connected!');
 
-    const answer = await inquirer.prompt([
-        {
-            name: 'testname',
-            type: 'input',
-            message: 'input here: ',
-        },
-    ]);
+    const onConnection = (newConnection: DataConnection) => {
+        if (ctx.connection) {
+            logError('Drop connection already exists!');
+            newConnection.close();
+            return;
+        }
 
-    console.log(answer);
-    peer.disconnect();
+        ctx.connection = newConnection;
+
+        currState = dropMachine.transition(DropState.Initial, {
+            type: DropEventType.Connect,
+            connection: ctx.connection,
+        });
+    };
+
+    ctx.peer.on('connection', onConnection);
+
+    const { id, nonce } = await post<InitDropResult, { id: string }>(
+        process.env.DEADDROP_API_URL! + DROP_API_PATH,
+        {
+            id: ctx.peer.id,
+        },
+    );
+
+    ctx.id = id;
+    ctx.nonce = nonce;
+
+    loader.start('Waiting for grab request...');
+
+    const initEvent: InitDropEvent = {
+        type: DropEventType.Init,
+        id,
+        peer: ctx.peer,
+        keyPair: ctx.keyPair,
+        nonce,
+    };
+
+    dropMachine.transition(currState!, initEvent);
+
+    cleanupPeer(ctx.peer);
 };
