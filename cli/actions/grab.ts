6294a32755163f8e2a471052d1f948d17476bb8a
Nieky Allen <nieky.allen@gmail.com>
2023-10-02 23:08:38 -0500

    progress on cli actions, update event transition logic

--- cli/actions/grab.ts
+++ cli/actions/grab.ts
@@ -1,15 +1,23 @@
 import { generateKeyPair } from '@shared/lib/crypto/operations';
 import { get } from '@shared/lib/fetch';
-import { initGrabContext } from '@shared/lib/machines/grab';
+import { grabMachine, initGrabContext } from '@shared/lib/machines/grab';
 import { DropDetails } from '@shared/types/common';
 import { loader } from 'lib/loader';
 import { displayWelcomeMessage, logError, logInfo } from 'lib/log';
 import { initPeer } from 'lib/peer';
 import { DROP_API_PATH } from '@shared/config/paths';
+import { AnyGrabEvent, InitGrabEvent } from '@shared/types/grab';
+import { cleanup } from 'lib/session';
+import { GrabEventType } from '@shared/lib/constants';
 
 export const grab = async (id: string) => {
     const ctx = initGrabContext();
-    let currState = null;
+    let currState = grabMachine.initialState;
+
+    const sendEvent = (event: AnyGrabEvent) => {
+        currState = grabMachine.transition(currState, event);
+        return currState;
+    };
 
     ctx.id = id;
 
@@ -38,11 +46,31 @@ export const grab = async (id: string) => {
 
     if (!details) {
         logError(`Drop instance ${ctx.id} not found, closing connection...`);
+        cleanup(ctx);
 
-        // cleanup();
         return;
     }
 
     ctx.dropperId = details.peerId;
     ctx.nonce = details.nonce;
+
+    const event: InitGrabEvent = {
+        type: GrabEventType.Init,
+        id: ctx.id,
+        dropperId: ctx.dropperId,
+        peer: ctx.peer,
+        keyPair: ctx.keyPair,
+        nonce: ctx.nonce,
+    };
+
+    sendEvent(event);
+
+    ctx.connection = ctx.peer!.connect(ctx.dropperId);
+
+    ctx.connection.on('error', console.error);
+    ctx.connection.on('open', () => {
+        logInfo('Drop connection successful...');
+
+        sendEvent({ type: GrabEventType.Connect });
+    });
 };
