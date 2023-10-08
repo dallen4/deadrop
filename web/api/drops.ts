7fcc991e7a90517f0a9cef59c2df21beb860c45a
Nieky Allen <nieky.allen@gmail.com>
2023-10-08 01:12:53 -0500

    update drop lib methods

--- web/api/drops.ts
+++ web/api/drops.ts
@@ -2,6 +2,31 @@ import { generateIV } from '@shared/lib/util';
 import { getRedis } from 'api/redis';
 import { formatDropKey } from 'lib/util';
 import { nanoid } from 'nanoid';
+import { generateDateTotalId } from './util';
+
+export const getDailyDropCount = async (target: Date) => {
+    const client = getRedis();
+
+    const dateId = generateDateTotalId(target);
+    const userDropCount = await client.get(dateId);
+
+    return userDropCount ? parseInt(userDropCount) : 0;
+};
+
+export const incrementDailyDropCount = async () => {
+    const client = getRedis();
+
+    const dateId = generateDateTotalId();
+
+    const dailyDropCount = await client.get(dateId);
+
+    let dailyCount = 1;
+
+    if (!dailyDropCount) await client.set(dateId, dailyCount);
+    else dailyCount = await client.incr(dateId);
+
+    return dailyCount;
+};
 
 const FIVE_MINS_IN_SEC = 10 * 60;
 
@@ -14,6 +39,8 @@ export const createDrop = async (peerId: string) => {
     const key = formatDropKey(dropId);
     await client.hset(key, { peerId, nonce });
     await client.expire(key, FIVE_MINS_IN_SEC);
+
+    await incrementDailyDropCount();
 
     return { dropId, nonce };
 };
