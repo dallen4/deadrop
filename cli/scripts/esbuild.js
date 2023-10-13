4e5b5a2be7f1e6da4ec631f1563eec9cba5f164f
Nieky Allen <nieky.allen@gmail.com>
2023-10-12 22:11:27 -0500

    update build flow for cli

---
+++ cli/scripts/esbuild.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+
+const { build } = require('esbuild');
+const { environmentPlugin } = require('esbuild-plugin-environment');
+
+(async () => {
+    await build({
+        entryPoints: ['index.ts'],
+        outfile: 'dist/deadrop.js',
+        platform: 'node',
+        bundle: true,
+        inject: ['./scripts/inject.js'],
+        loader: { '.node': 'file' },
+        plugins: [
+            environmentPlugin({
+                DEADDROP_API_URL: process.env.DEADDROP_API_URL,
+                PEER_SERVER_URL: process.env.PEER_SERVER_URL,
+            }),
+        ],
+    }).catch((err) => {
+        process.stderr.write(err.stderr);
+        process.exit(1);
+    });
+})();
