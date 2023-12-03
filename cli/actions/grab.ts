e6796949a47fda57e0573b7e19c362d080d597b8
Nieky Allen <nieky.allen@gmail.com>
2023-12-03 01:52:56 -0600

    cleanup imports

--- cli/actions/grab.ts
+++ cli/actions/grab.ts
@@ -1,37 +1,9 @@
-import {
-    decryptRaw,
-    deriveKey,
-    exportKey,
-    generateKeyPair,
-    hashRaw,
-    importKey,
-} from '@shared/lib/crypto/operations';
-import { get } from '@shared/lib/fetch';
 import { grabMachine, initGrabContext } from '@shared/lib/machines/grab';
-import { DropDetails } from '@shared/types/common';
-import { loader } from 'lib/loader';
 import { displayWelcomeMessage, logDebug, logError, logInfo } from 'lib/log';
 import { initPeer } from 'lib/peer';
-import { DROP_API_PATH } from '@shared/config/paths';
-import {
-    AckHandshakeEvent,
-    AnyGrabEvent,
-    InitGrabEvent,
-} from '@shared/types/grab';
+import { AnyGrabEvent } from '@shared/types/grab';
 import { cleanupSession } from 'lib/session';
-import {
-    GrabEventType,
-    GrabMessageOrderMap,
-    MessageType,
-} from '@shared/lib/constants';
-import { withMessageLock } from '@shared/lib/messages';
-import {
-    BaseMessage,
-    ConfirmIntegrityMessage,
-    DropMessage,
-    HandshakeMessage,
-    VerifyMessage,
-} from '@shared/types/messages';
+import { MessageType } from '@shared/lib/constants';
 import { decryptFile, hashFile } from 'lib/crypto';
 import { createGrabHandlers } from '@shared/handlers/grab';
 
