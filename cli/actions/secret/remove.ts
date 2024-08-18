8120d571502f688c806f70e0fd8fe0468e6b7c57
Nieky Allen <nieky.allen@gmail.com>
2024-08-18 10:30:01 -0700

    feat: create actions/ dir for sub-command implementations, first draft of secret and vault commands

---
+++ cli/actions/secret/remove.ts
@@ -0,0 +1,17 @@
+import { createSecretsHelpers } from 'db/secrets';
+import { loadConfig } from 'lib/config';
+import { logInfo } from 'lib/log';
+
+export async function secretRemove(name: string) {
+  logInfo('removing secret to vault...');
+
+  const { config } = await loadConfig();
+
+  const { vaults, active_vault } = config;
+
+  const { removeSecret } = createSecretsHelpers(vaults[active_vault]);
+
+  await removeSecret(name);
+
+  logInfo('secret removed successfully!');
+}
---
+++ cli/actions/vault/delete.ts
@@ -0,0 +1,44 @@
+import { confirm } from '@inquirer/prompts';
+import { snakeCase } from 'change-case';
+import { vaultExists } from 'db/vaults';
+import { existsSync } from 'fs';
+import { unlink } from 'fs/promises';
+import { loadConfig, saveConfig } from 'lib/config';
+import { logError, logInfo } from 'lib/log';
+import { cwd, exit } from 'process';
+
+export async function vaultDelete(vaultNameInput: string) {
+  const { config } = await loadConfig();
+
+  const { vaults, active_vault } = config;
+
+  const vaultName = snakeCase(vaultNameInput);
+
+  if (!vaultExists(vaults, vaultName)) {
+    logError('Vault not found!');
+    return exit(1);
+  }
+
+  const shouldDelete = await confirm({
+    message: `Are you sure you want to delete '${vaultName}' vault?`,
+  });
+
+  if (shouldDelete) {
+    if (vaultName === active_vault) {
+      logInfo(
+        `User vault to be deleted cannot be selected as active. Switch to a different vault and try again.`,
+      );
+
+      return exit(1);
+    }
+    const { location } = vaults[vaultName];
+
+    await unlink(location);
+
+    delete vaults[vaultName];
+
+    await saveConfig(cwd(), { active_vault, vaults }, true);
+
+    logInfo(`Vault '${vaultName}' deleted succesfully!`);
+  }
+}
