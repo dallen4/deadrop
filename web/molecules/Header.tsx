88780fff2bbf1516c2c3434ce0839be5c6b2aba9
Nieky Allen <nieky.allen@gmail.com>
2024-09-20 12:27:09 -0500

    chore: remove login button from header -- will reimpl auth w/ clerk over auth0

--- web/molecules/Header.tsx
+++ web/molecules/Header.tsx
@@ -1,70 +1,63 @@
-import React from 'react';
 import {
-    Avatar,
-    Header as BaseHeader,
-    Box,
-    Button,
-    Flex,
-    Group,
-    Loader,
-    Popover,
-    createStyles,
+  Header as BaseHeader,
+  Box,
+  Button,
+  Flex,
+  Group,
+  createStyles,
 } from '@mantine/core';
-import Brand from 'atoms/header/Brand';
-import { useUser } from '@auth0/nextjs-auth0/client';
-import { useRouter } from 'next/router';
-import {
-    LOGIN_PATH,
-    LOGOUT_PATH,
-    OVERVIEW_DOCS_PATH,
-} from '@shared/config/paths';
 import { useMediaQuery } from '@mantine/hooks';
+import { OVERVIEW_DOCS_PATH } from '@shared/config/paths';
+import Brand from 'atoms/header/Brand';
+import { useRouter } from 'next/router';
 
 const useStyles = createStyles((theme) => ({
-    navButton: {
-        fontWeight: 'bold',
-    },
+  navButton: {
+    fontWeight: 'bold',
+  },
 }));
 
 const Header = () => {
-    const router = useRouter();
-    const { user, isLoading } = useUser();
-    const { classes, theme } = useStyles();
-    const isMobile = useMediaQuery(`(max-width: ${theme.breakpoints.sm}px)`);
+  const router = useRouter();
+  // const { user, isLoading } = useUser();
+  const { classes, theme } = useStyles();
+  const isMobile = useMediaQuery(
+    `(max-width: ${theme.breakpoints.sm}px)`,
+  );
 
-    const onLogin = () => router.push(LOGIN_PATH);
+  // const onLogin = () => router.push(LOGIN_PATH);
 
-    const onDocsClick = () => router.push(OVERVIEW_DOCS_PATH);
+  const onDocsClick = () => router.push(OVERVIEW_DOCS_PATH);
 
-    return (
-        <BaseHeader
-            height={102}
-            withBorder={false}
-            styles={(theme) => ({
-                root: {
-                    padding: isMobile ? theme.spacing.md : theme.spacing.xl,
-                },
-            })}
+  return (
+    <BaseHeader
+      height={102}
+      withBorder={false}
+      styles={(theme) => ({
+        root: {
+          padding: isMobile ? theme.spacing.md : theme.spacing.xl,
+        },
+      })}
+    >
+      <Flex direction="row">
+        <Box
+          style={{
+            flex: 1,
+            justifyContent: 'flex-start',
+            alignItems: 'center',
+          }}
         >
-            <Flex direction="row">
-                <Box
-                    style={{
-                        flex: 1,
-                        justifyContent: 'flex-start',
-                        alignItems: 'center',
-                    }}
-                >
-                    <Brand />
-                </Box>
-                <Group ml={50}>
-                    <Button
-                        variant={'subtle'}
-                        className={classes.navButton}
-                        onClick={onDocsClick}
-                    >
-                        Docs
-                    </Button>
-                    {user ? (
+          <Brand />
+        </Box>
+        <Group ml={50}>
+          <Button
+            variant={'subtle'}
+            className={classes.navButton}
+            onClick={onDocsClick}
+          >
+            Docs
+          </Button>
+          {/* {user ? (
                         <Popover position={'bottom-end'}>
                             <Popover.Target>
                                 <Avatar
@@ -95,11 +88,11 @@ const Header = () => {
                                 'Login'
                             )}
                         </Button>
-                    )}
-                </Group>
-            </Flex>
-        </BaseHeader>
-    );
+                    )} */}
+        </Group>
+      </Flex>
+    </BaseHeader>
+  );
 };
 
 export default Header;
