aca7563237979001bf6be5f9b6a5679ce5bd19d0
Nieky Allen <nieky.allen@gmail.com>
2024-10-04 02:26:13 -0500

    chore: cleanup pipeline name, formatting, add zod & ts-prune

--- web/pages/api/captcha.ts
+++ web/pages/api/captcha.ts
@@ -3,20 +3,23 @@ import { runMiddleware } from 'api/middleware';
 import { cors } from 'api/middleware/cors';
 import { verifyCaptcha } from 'api/captcha';
 
-export default async function drop(req: NextApiRequest, res: NextApiResponse) {
-    await runMiddleware(req, res, cors);
+export default async function verifyCatpcha(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  await runMiddleware(req, res, cors);
 
-    if (req.method !== 'POST') {
-        res.setHeader('Allow', 'POST');
-        res.status(405).end('Method Not Allowed');
-        return;
-    }
+  if (req.method !== 'POST') {
+    res.setHeader('Allow', 'POST');
+    res.status(405).end('Method Not Allowed');
+    return;
+  }
 
-    const { token } = req.body;
+  const { token } = req.body;
 
-    const isValid = await verifyCaptcha(token);
+  const isValid = await verifyCaptcha(token);
 
-    return isValid
-        ? res.status(200).send({ success: true })
-        : res.status(400).send({ success: false });
+  return isValid
+    ? res.status(200).send({ success: true })
+    : res.status(400).send({ success: false });
 }
