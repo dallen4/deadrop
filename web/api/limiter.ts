50e2a916e05ab0af2582bc00717364e6e5c91170
Nieky Allen <nieky.allen@gmail.com>
2024-10-04 01:53:15 -0500

    feat: cleanup dropc ount parsing

--- web/api/limiter.ts
+++ web/api/limiter.ts
@@ -1,5 +1,7 @@
 import { hashRaw } from '@shared/lib/crypto/operations';
 import { getRedis } from './redis';
+import { get } from '@vercel/edge-config';
+import { DAILY_DROP_LIMIT_COOKIE } from '@config/cookies';
 
 const DAY_IN_SEC = 60 * 60 * 24;
 
@@ -13,7 +15,9 @@ export const checkAndIncrementDropCount = async (ipAddress: string) => {
     if (!userDropCount) {
         await client.setex(userIpHash, DAY_IN_SEC, 1);
     } else {
-        if (parseInt(userDropCount) >= 5)
+        const dailyDropLimit = await get<number>(DAILY_DROP_LIMIT_COOKIE);
+
+        if (parseInt(userDropCount) >= dailyDropLimit!)
             return false;
         else await client.incr(userIpHash);
     }
