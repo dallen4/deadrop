d5998497f73fa2651254d6bb5278382c52f48d27
Nieky Allen <nieky.allen@gmail.com>
2024-10-10 23:03:05 -0500

    chore: add margin to drop logs toggle button

--- web/molecules/DropLog.tsx
+++ web/molecules/DropLog.tsx
@@ -2,28 +2,29 @@ import React, { useState } from 'react';
 import { Button, Code, Collapse, Text } from '@mantine/core';
 
 const DropLog = ({ logs }: { logs: Array<string> }) => {
-    const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false);
 
-    return (
-        <div>
-            <Text>Drop Logs</Text>
-            <Button
-                compact
-                size={'sm'}
-                variant={'light'}
-                onClick={() => setOpen((prev) => !prev)}
-            >
-                {open ? 'hide' : 'show'}
-            </Button>
-            <Collapse in={open}>
-                <Code block>
-                    {logs.map((log, index) => (
-                        <div key={index}>{log}</div>
-                    ))}
-                </Code>
-            </Collapse>
-        </div>
-    );
+  return (
+    <div>
+      <Text>Drop Logs</Text>
+      <Button
+        compact
+        size={'sm'}
+        my={'sm'}
+        variant={'light'}
+        onClick={() => setOpen((prev) => !prev)}
+      >
+        {open ? 'hide' : 'show'}
+      </Button>
+      <Collapse in={open}>
+        <Code block>
+          {logs.map((log, index) => (
+            <div key={index}>{log}</div>
+          ))}
+        </Code>
+      </Collapse>
+    </div>
+  );
 };
 
 export default DropLog;
