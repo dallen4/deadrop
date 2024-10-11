d6d9dbcd52e0f0525aa8be51a5be3cb6b1b2de33
Nieky Allen <nieky.allen@gmail.com>
2024-10-10 23:03:05 -0500

    chore: make hcaptcha element dark & compact

--- web/atoms/Captcha.tsx
+++ web/atoms/Captcha.tsx
@@ -4,30 +4,31 @@ import { post } from '@shared/lib/fetch';
 import { CAPTCHA_API_PATCH } from '@shared/config/paths';
 
 export const Captcha = ({ onSuccess, onExpire }: CaptchaProps) => {
-    const onVerify = async (token: string, _ekey: string) => {
-        const resp = await post<{ success: boolean }, { token: string }>(
-            CAPTCHA_API_PATCH,
-            { token },
-        );
-
-        if (resp.success) onSuccess();
-    };
-
-    const onError = (event: string) => {
-        console.warn(event);
-    };
-
-    return (
-        <HCaptcha
-            sitekey={process.env.NEXT_PUBLIC_HCAPTCHA_SITEKEY!}
-            onVerify={onVerify}
-            onError={onError}
-            onExpire={onExpire}
-        />
+  const onVerify = async (token: string, _ekey: string) => {
+    const resp = await post<{ success: boolean }, { token: string }>(
+      CAPTCHA_API_PATCH,
+      { token },
     );
+
+    if (resp.success) onSuccess();
+  };
+
+  const onError = (event: string) => {
+    console.warn(event);
+  };
+
+  return (
+    <HCaptcha
+      sitekey={process.env.NEXT_PUBLIC_HCAPTCHA_SITEKEY!}
+      theme={'dark'}
+      onVerify={onVerify}
+      onError={onError}
+      onExpire={onExpire}
+    />
+  );
 };
 
 type CaptchaProps = {
-    onSuccess: () => void;
-    onExpire: () => void;
+  onSuccess: () => void;
+  onExpire: () => void;
 };
