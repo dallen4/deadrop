64888cc899eed612af55f1e252b51d0e530af914
Nieky Allen <nieky.allen@gmail.com>
2024-10-10 23:03:05 -0500

    fix: update styling to avoid footer overlay on mobile

--- web/pages/grab.tsx
+++ web/pages/grab.tsx
@@ -3,11 +3,14 @@ import GrabFlow from 'organisms/GrabFlow';
 import { Container } from '@mantine/core';
 
 const Grab = () => {
-    return (
-        <Container style={{ maxWidth: '700px' }} p={0}>
-            <GrabFlow />
-        </Container>
-    );
+  return (
+    <Container
+      style={{ maxWidth: '700px', minHeight: 'calc(100vh - 202px)' }}
+      p={0}
+    >
+      <GrabFlow />
+    </Container>
+  );
 };
 
 export default Grab;
