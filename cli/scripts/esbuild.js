d0aaac6b3719df7a2f0efafd230b2a6c7815b124
Nieky Allen <nieky.allen@gmail.com>
2024-10-31 20:44:07 -0500

    fix: update build config to support open lib

--- cli/scripts/esbuild.js
+++ cli/scripts/esbuild.js
@@ -4,28 +4,34 @@ const { build } = require('esbuild');
 const { environmentPlugin } = require('esbuild-plugin-environment');
 
 if (!process.env.DEADDROP_API_URL || !process.env.PEER_SERVER_URL) {
-    console.error('Invalid environment configuration provided');
-    process.exit(1);
+  console.error('Invalid environment configuration provided');
+  process.exit(1);
 }
 
 (async () => {
-    await build({
-        entryPoints: ['index.ts'],
-        outfile: 'dist/deadrop.js',
-        platform: 'node',
-        bundle: true,
-        inject: ['./scripts/inject.js'],
-        loader: { '.node': 'file' },
-        external: ['libsql'],
-        plugins: [
-            environmentPlugin({
-                DEADDROP_API_URL: process.env.DEADDROP_API_URL,
-                PEER_SERVER_URL: process.env.PEER_SERVER_URL,
-                CLERK_PUBLISHABLE_KEY: process.env.CLERK_PUBLISHABLE_KEY,
-            }),
-        ],
-    }).catch((err) => {
-        process.stderr.write(err.stderr);
-        process.exit(1);
-    });
+  await build({
+    entryPoints: ['index.ts'],
+    outfile: 'dist/deadrop.js',
+    platform: 'node',
+    bundle: true,
+    inject: ['./scripts/inject.js'],
+    loader: { '.node': 'file' },
+    external: ['libsql'],
+    plugins: [
+      environmentPlugin({
+        DEADDROP_API_URL: process.env.DEADDROP_API_URL,
+        PEER_SERVER_URL: process.env.PEER_SERVER_URL,
+        CLERK_PUBLISHABLE_KEY: process.env.CLERK_PUBLISHABLE_KEY,
+      }),
+    ],
+
+    // ref: https://github.com/sindresorhus/open/issues/330#issuecomment-2047513034
+    define: { 'import.meta.url': '_importMetaUrl' },
+    banner: {
+      js: "const _importMetaUrl=require('url').pathToFileURL(__filename)",
+    },
+  }).catch((err) => {
+    process.stderr.write(err.stderr);
+    process.exit(1);
+  });
 })();
