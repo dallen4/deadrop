dd48e7059b51e6ef118557feb304b1cc22656a6f
Nieky Allen <nieky.allen@gmail.com>
2024-11-01 02:32:04 -0500

    feat: cli auth and worker cleanup

--- cli/lib/auth/localhostServer.ts
+++ cli/lib/auth/localhostServer.ts
@@ -7,18 +7,49 @@ import { Server, Socket } from 'net';
 const PATH = '/';
 
 const successHtml = html`
-  <body>
-    <main
-      style="height: 100%; width: 100%; display: flex;justify-content: center;align-items: center"
-    >
-      <div>
-        <h1>Signed in</h1>
-        <h2>
-          You can safely close this tab and return to your terminal
-        </h2>
-      </div>
-    </main>
-  </body>
+  <html>
+    <head>
+      <title>Login Success!</title>
+      <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/@mantine/core@7.13.4/styles.min.css"
+      />
+
+      <style>
+        body {
+          margin: 0;
+          padding: 0;
+        }
+
+        main {
+          background-color: #1a1b1e;
+          height: 100%;
+          width: 100%;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+
+        h1 {
+          color: #1971c2;
+        }
+
+        h2 {
+          color: rgb(248, 249, 250);
+        }
+      </style>
+    </head>
+    <body>
+      <main>
+        <div>
+          <h1 class="styles-Text-root styles-Title-root">You're all set!</h1>
+          <h2 class="styles-Text-root">
+            You can safely close this tab and return to your terminal
+          </h2>
+        </div>
+      </main>
+    </body>
+  </html>
 `;
 
 // Force destroy server + open connections
--- worker/worker-configuration.d.ts
+++ worker/worker-configuration.d.ts
@@ -3,4 +3,14 @@ interface Env {
 
   // Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
   PEER_SERVER: DurableObjectNamespace;
+
+  // variables
+  DAILY_DROP_LIMIT: number;
+
+  // secrets
+  CLERK_SECRET_KEY: string;
+  CLERK_PUBLISHABLE_KEY: string;
+
+  UPSTASH_REDIS_REST_URL: string;
+  UPSTASH_REDIS_REST_TOKEN: string;
 }
