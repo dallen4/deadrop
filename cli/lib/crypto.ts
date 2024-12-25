927c44c1c77e65daa7b266dc7ac8faa6d384304b
Nieky Allen <nieky.allen@gmail.com>
2024-12-25 00:50:09 -0600

    feat: create deadrop client factory util, implement wrap and unwrap methods

--- cli/lib/crypto.ts
+++ cli/lib/crypto.ts
@@ -1,11 +1,16 @@
 import type { DropMessage } from '@shared/types/messages';
 import {
   decrypt,
+  decryptRaw,
   encrypt,
+  encryptRaw,
   hash,
+  importKeyFromBase64,
 } from '@shared/lib/crypto/operations';
 import { writeFileFromBuffer, readFileAsBuffer } from './files';
 import Mime from 'mime-type';
+import { generateIV } from '@shared/lib/util';
+import { SECRET_VALUE_DELIMITER } from './constants';
 
 export const encryptFile = async (
   key: CryptoKey,
@@ -41,3 +46,26 @@ export const hashFile = async (path: string) => {
   const fileAsArrayBuffer = await readFileAsBuffer(path);
   return hash(fileAsArrayBuffer);
 };
+
+export async function wrapSecret(key: string, value: string) {
+  const iv = generateIV();
+
+  const encryptionKey = await importKeyFromBase64(key, ['encrypt']);
+
+  const encryptedSecret = await encryptRaw(encryptionKey, iv, value);
+
+  return [iv, encryptedSecret].join(SECRET_VALUE_DELIMITER);
+}
+
+export async function unwrapSecret(
+  key: string,
+  wrappedSecret: string,
+) {
+  const [iv, encryptedValue] = wrappedSecret.split(
+    SECRET_VALUE_DELIMITER,
+  );
+
+  const decryptionKey = await importKeyFromBase64(key, ['decrypt']);
+
+  return decryptRaw(decryptionKey, iv, encryptedValue);
+}
