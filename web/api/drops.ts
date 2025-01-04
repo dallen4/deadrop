2d08801c67b5a00dd700786f9b1604622c4ec2bd
Nieky Allen <nieky.allen@gmail.com>
2025-01-04 03:20:03 -0600

    chore: update deps, refactor to use @upstash/redis

--- web/api/drops.ts
+++ web/api/drops.ts
@@ -6,57 +6,57 @@ import { generateDateTotalId } from './util';
 import { DropDetails } from '@shared/types/common';
 
 export const getDailyDropCount = async (target: Date) => {
-    const client = getRedis();
+  const client = getRedis();
 
-    const dateId = generateDateTotalId(target);
-    const userDropCount = await client.get(dateId);
+  const dateId = generateDateTotalId(target);
+  const userDropCount = await client.get<number>(dateId);
 
-    return userDropCount ? parseInt(userDropCount) : 0;
+  return userDropCount ? userDropCount : 0;
 };
 
 export const incrementDailyDropCount = async () => {
-    const client = getRedis();
+  const client = getRedis();
 
-    const dateId = generateDateTotalId();
+  const dateId = generateDateTotalId();
 
-    const dailyDropCount = await client.get(dateId);
+  const dailyDropCount = await client.get(dateId);
 
-    let dailyCount = 1;
+  let dailyCount = 1;
 
-    if (!dailyDropCount) await client.set(dateId, dailyCount);
-    else dailyCount = await client.incr(dateId);
+  if (!dailyDropCount) await client.set(dateId, dailyCount);
+  else dailyCount = await client.incr(dateId);
 
-    return dailyCount;
+  return dailyCount;
 };
 
 const FIVE_MINS_IN_SEC = 10 * 60;
 
 export const createDrop = async (peerId: string) => {
-    const client = getRedis();
+  const client = getRedis();
 
-    const dropId = nanoid();
-    const nonce = generateIV();
+  const dropId = nanoid();
+  const nonce = generateIV();
 
-    const key = formatDropKey(dropId);
-    await client.hset(key, { peerId, nonce });
-    await client.expire(key, FIVE_MINS_IN_SEC);
+  const key = formatDropKey(dropId);
+  await client.hset(key, { peerId, nonce });
+  await client.expire(key, FIVE_MINS_IN_SEC);
 
-    await incrementDailyDropCount();
+  await incrementDailyDropCount();
 
-    return { dropId, nonce };
+  return { dropId, nonce };
 };
 
 export const getDrop = async (id: string): Promise<DropDetails> => {
-    const client = getRedis();
-    const dropItem = await client.hgetall(formatDropKey(id));
+  const client = getRedis();
+  const dropItem = await client.hgetall(formatDropKey(id));
 
-    return dropItem as DropDetails;
+  return dropItem as DropDetails;
 };
 
 export const deleteDrop = async (id: string): Promise<boolean> => {
-    const client = getRedis();
-    const key = formatDropKey(id);
-    await client.del(key);
+  const client = getRedis();
+  const key = formatDropKey(id);
+  await client.del(key);
 
-    return true;
+  return true;
 };
