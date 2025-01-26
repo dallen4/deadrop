80bcf51562940071ab202dc746faa24a6b700564
Nieky Allen <nieky.allen@gmail.com>
2025-01-26 05:09:19 -0600

    fix: include cookies with hono client

--- shared/client.ts
+++ shared/client.ts
@@ -4,6 +4,13 @@ import { DeadropWorkerApi } from '@api/client';
 export const createClient = (
   url: string,
   options?: ClientRequestOptions,
-) => hc<DeadropWorkerApi>(url, options);
+) =>
+  hc<DeadropWorkerApi>(url, {
+    ...options,
+    init: {
+      credentials: 'include',
+      ...options?.init,
+    },
+  });
 
 export type DeadropApiClient = ReturnType<typeof createClient>;
