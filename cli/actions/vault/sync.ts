dbb092aebd48af597dc7c5b936cf2be4987f9502
Nieky Allen <nieky.allen@gmail.com>
2025-01-28 01:04:12 -0600

    chore: move secret helper methods to shared workspace

--- cli/actions/vault/sync.ts
+++ cli/actions/vault/sync.ts
@@ -1,5 +1,5 @@
 import { initDBClient } from 'db/init';
-import { createSecretsHelpers } from 'db/secrets';
+import { createSecretsHelpers } from '@shared/db/secrets';
 import { loadConfig } from 'lib/config';
 import { syncEnv } from 'lib/env';
 import { logInfo } from 'lib/log';
@@ -20,7 +20,7 @@ export async function vaultSync(
     activeVault.cloud,
   );
 
-  const { getAllSecrets } = await createSecretsHelpers(
+  const { getAllSecrets } = createSecretsHelpers(
     vaults[active_vault.name],
     db,
   );
