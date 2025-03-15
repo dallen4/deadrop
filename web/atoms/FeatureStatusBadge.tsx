8f860221fd9d6338968244d9bbe20426a2070381
Nieky Allen <nieky.allen@gmail.com>
2025-03-14 23:55:14 -0500

    chore: cleanup and featureStatusBadge abstraction

---
+++ web/atoms/FeatureStatusBadge.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Badge } from '@mantine/core/lib/Badge';
+
+export type FeatureStatus =
+  | 'stable'
+  | 'experimental'
+  | 'in development';
+
+const statusToColor: Record<FeatureStatus, string> = {
+  stable: 'blue',
+  experimental: 'indigo',
+  'in development': 'yellow',
+};
+
+export const FeatureStatusBadge = ({
+  status,
+}: {
+  status: keyof typeof statusToColor;
+}) => {
+  return (
+    <Badge
+      size={'lg'}
+      color={statusToColor[status as keyof typeof statusToColor]}
+    >
+      {status}
+    </Badge>
+  );
+};
--- web/molecules/steps/SecretInputCard.tsx
+++ web/molecules/steps/SecretInputCard.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react';
+import React, { useRef, useState } from 'react';
 import {
   Box,
   Button,
