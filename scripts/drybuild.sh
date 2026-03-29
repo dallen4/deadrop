717fa47d10ba7d72ff11c3f19e55ef071bab449c
Nieky Allen <nieky.allen@gmail.com>
2026-03-29 01:13:29 -0500

    chore: update wt config & impl new scripts

--- scripts/drybuild.sh
+++ scripts/drybuild.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # build CLI
-yarn cli:build
+pnpm cli:build
 
 # dry run of deploy compilation worker
-yarn worker:deploy --dry-run
+pnpm worker:deploy --dry-run
 
 # build web app asssets
-yarn build
+pnpm build
