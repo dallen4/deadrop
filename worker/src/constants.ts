d09c88a70175cd0b0080511d6aeb6630b22cd3fa
Nieky Allen <nieky.allen@gmail.com>
2026-05-10 22:09:41 -0500

    fix: convert route enums to const objects and migrate to c.var.clerkAuth()

--- worker/src/constants.ts
+++ worker/src/constants.ts
@@ -9,42 +9,47 @@ export enum ContentType {
   Json = 'application/json',
 }
 
-export enum AppRouteParts {
-  Root = '/',
-  Drop = '/drop',
+export type AppRouteParts =
+  (typeof AppRouteParts)[keyof typeof AppRouteParts];
+
+export const AppRouteParts = {
+  Root: '/',
+  Drop: '/drop',
 
   // auth
-  AuthRoot = '/auth',
-  CreateSignInToken = '/token',
-  Me = '/me',
+  AuthRoot: '/auth',
+  CreateSignInToken: '/token',
+  Me: '/me',
 
   // peerjs
-  PeerJsRoot = '/peerjs',
-  GenerateId = `/id`,
+  PeerJsRoot: '/peerjs',
+  GenerateId: '/id',
 
   // vaults
-  VaultRoot = '/vault',
-  NameParam = '/:name',
-  Share = `${AppRouteParts.NameParam}/share`,
+  VaultRoot: '/vault',
+  NameParam: '/:name',
+  Share: '/:name/share',
 
   // TODO not implemented
-  VaultExists = `${AppRouteParts.NameParam}/exists`,
-}
+  VaultExists: '/:name/exists',
+} as const;
 
-export enum AppRoutes {
-  Root = AppRouteParts.Root,
-  Drop = AppRouteParts.Drop,
+export type AppRoutes = (typeof AppRoutes)[keyof typeof AppRoutes];
+
+export const AppRoutes = {
+  Root: AppRouteParts.Root,
+  Drop: AppRouteParts.Drop,
 
   // auth
-  AuthRoot = AppRouteParts.AuthRoot,
-  AuthCreateSignInToken = `${AppRouteParts.AuthRoot}${AppRouteParts.CreateSignInToken}`,
-  AuthMe = `${AppRouteParts.AuthRoot}${AppRouteParts.Me}`,
+  AuthRoot: AppRouteParts.AuthRoot,
+  AuthCreateSignInToken: `${AppRouteParts.AuthRoot}${AppRouteParts.CreateSignInToken}` as const,
+  AuthMe: `${AppRouteParts.AuthRoot}${AppRouteParts.Me}` as const,
 
   // peerjs paths
-  PeerJsRoot = AppRouteParts.PeerJsRoot,
-  PeerJsGenerateId = `${AppRouteParts.PeerJsRoot}${AppRouteParts.GenerateId}`,
+  PeerJsRoot: AppRouteParts.PeerJsRoot,
+  PeerJsGenerateId: `${AppRouteParts.PeerJsRoot}${AppRouteParts.GenerateId}` as const,
 
   // vault paths
-  VaultRoot = AppRouteParts.VaultRoot,
-  ShareVault = `${AppRouteParts.VaultRoot}${AppRouteParts.NameParam}${AppRouteParts.Share}`,
-}
+  VaultRoot: AppRouteParts.VaultRoot,
+  ShareVault: `${AppRouteParts.VaultRoot}${AppRouteParts.NameParam}${AppRouteParts.Share}` as const,
+} as const;
