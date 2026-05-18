f3084ebabbfc27bc2462fe6d324ed79de6031d58
Nieky Allen <nieky.allen@gmail.com>
2026-05-17 23:47:09 -0500

    feat(docs): sidebar layout, mobile drawer, MDX conditional wrapper

--- shared/config/paths.ts
+++ shared/config/paths.ts
@@ -6,7 +6,15 @@ export const GRAB_PATH = '/grab';
 
 export const OVERVIEW_DOCS_PATH = '/docs/overview';
 
-export const CLI_DOCS_PATH = '/docs/cli';
+export const CLI_DOCS_PATH = '/docs/features/cli';
+
+export const WEB_DOCS_PATH = '/docs/features/web';
+
+export const VSCODE_DOCS_PATH = '/docs/features/vscode';
+
+export const FAQS_DOCS_PATH = '/docs/faqs';
+
+export const FEATURES_DOCS_PATH = '/docs/features';
 
 export const CAPTCHA_API_PATH = '/api/captcha';
 
--- web/mdx-components.tsx
+++ web/mdx-components.tsx
@@ -1,204 +1,211 @@
 import { MAX_MDX_CONTENT_WIDTH } from '@config/app';
 import {
-    Blockquote,
-    Code,
-    Title,
-    Text,
-    List,
-    Divider,
-    Box,
-    Center,
-    useMantineTheme,
-    Anchor,
+  Anchor,
+  Blockquote,
+  Box,
+  Center,
+  Divider,
+  List,
+  PolymorphicComponentProps,
+  Text,
+  TextProps,
+  Title,
+  useMantineTheme,
 } from '@mantine/core';
-import { InlineCode } from 'atoms/Code';
+import { BlockCode, InlineCode } from 'atoms/Code';
+import { useMobile } from 'hooks/use-mobile';
 import type { MDXComponents } from 'mdx/types';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { AnchorHTMLAttributes, DetailedHTMLProps } from 'react';
 
-export function useMDXComponents(components: MDXComponents): MDXComponents {
-    const theme = useMantineTheme();
-    const { pathname } = useRouter();
-    const isDocs = pathname.startsWith('/docs');
+export function useMDXComponents(
+  components: MDXComponents,
+): MDXComponents {
+  const theme = useMantineTheme();
+  const isMobile = useMobile();
+  const { pathname } = useRouter();
+  const isDocs = pathname.startsWith('/docs');
 
-    const LargeText = (props: any) => (
-        <Text size={theme.fontSizes.lg} {...props} />
+  const LargeText = (
+    props: PolymorphicComponentProps<'p' | 'span', TextProps>,
+  ) => <Text size={'lg'} {...props} />;
+
+  const DynamicLink = ({
+    href,
+    children,
+  }: DetailedHTMLProps<
+    AnchorHTMLAttributes<HTMLAnchorElement>,
+    HTMLAnchorElement
+  >) => {
+    const props: any = {};
+
+    if (href!.startsWith('/')) props.component = Link;
+    else props.target = '_blank';
+
+    return (
+      <Anchor href={href!} size={'lg'} {...props}>
+        {children}
+      </Anchor>
     );
+  };
 
-    const DynamicLink = ({
-        href,
-        children,
-    }: DetailedHTMLProps<
-        AnchorHTMLAttributes<HTMLAnchorElement>,
-        HTMLAnchorElement
-    >) => {
-        const props: any = {};
+  const customComponents: MDXComponents = {
+    a: ({ href, children }) => (
+      <DynamicLink href={href}>{children}</DynamicLink>
+    ),
+    blockquote: ({ children }) => (
+      <Blockquote
+        color={'blue'}
+        mt={'md'}
+        mb={'lg'}
+        pl={'lg'}
+        fz={'lg'}
+      >
+        {children}
+      </Blockquote>
+    ),
+    code: ({ children }) =>
+      typeof children === 'string' && !children.includes('\n') ? (
+        <InlineCode size={'md'}>{children}</InlineCode>
+      ) : (
+        <BlockCode>{children}</BlockCode>
+      ),
+    em: ({ children }) => (
+      <LargeText component={'span'} fs={'italic'}>
+        {children}
+      </LargeText>
+    ),
+    h1: ({ children }) => (
+      <Title
+        order={1}
+        style={{
+          paddingTop: theme.spacing.md,
+          paddingBottom: theme.spacing.md,
+        }}
+      >
+        {children}
+      </Title>
+    ),
+    h2: ({ children }) => (
+      <Title
+        order={2}
+        style={{
+          paddingTop: theme.spacing.xl,
+          paddingBottom: theme.spacing.sm,
+          borderBottom: `1px solid light-dark(${theme.colors.gray[2]}, ${theme.colors.dark[4]})`,
+          marginBottom: theme.spacing.sm,
+        }}
+      >
+        {children}
+      </Title>
+    ),
+    h3: ({ children }) => (
+      <Title
+        order={3}
+        style={{
+          paddingTop: theme.spacing.lg,
+          paddingBottom: theme.spacing.xs,
+        }}
+      >
+        {children}
+      </Title>
+    ),
+    h4: ({ children }) => (
+      <Title
+        order={4}
+        style={{
+          paddingTop: theme.spacing.md,
+          paddingBottom: theme.spacing.xs,
+        }}
+      >
+        {children}
+      </Title>
+    ),
+    h5: ({ children }) => (
+      <Title
+        order={5}
+        style={{
+          paddingTop: theme.spacing.sm,
+          paddingBottom: theme.spacing.xs,
+        }}
+      >
+        {children}
+      </Title>
+    ),
+    h6: ({ children }) => (
+      <Title
+        order={6}
+        style={{
+          paddingTop: theme.spacing.sm,
+          paddingBottom: theme.spacing.xs,
+        }}
+      >
+        {children}
+      </Title>
+    ),
+    hr: () => <Divider my={'lg'} />,
+    li: ({ children }) => (
+      <List.Item
+        style={{
+          paddingBottom: 'calc(var(--mantine-spacing-xs) * 0.3)',
+          fontSize: 'lg',
+        }}
+      >
+        {children}
+      </List.Item>
+    ),
+    ol: ({ children }) => (
+      <List mb={'md'} type={'ordered'}>
+        {children}
+      </List>
+    ),
+    p: ({ children }) => (
+      <LargeText
+        styles={{
+          root: {
+            lineHeight: 1.5,
+          },
+        }}
+      >
+        {children}
+      </LargeText>
+    ),
+    strong: ({ children }) => (
+      <LargeText component={'span'} fw={700}>
+        {children}
+      </LargeText>
+    ),
+    ul: ({ children }) => (
+      <List mb={'md'} type={'unordered'}>
+        {children}
+      </List>
+    ),
+    wrapper: ({ children }) => (
+      <Center
+        style={{
+          marginBottom: 'calc(var(--mantine-spacing-xl) * 1.25)',
+          marginTop: isDocs ? undefined : 'var(--mantine-spacing-xl)',
+          paddingLeft:
+            isDocs && !isMobile ? theme.spacing.xl : undefined,
+          paddingRight:
+            isDocs && !isMobile ? theme.spacing.xl : undefined,
+        }}
+      >
+        <Box
+          style={{
+            width: '100%',
+            maxWidth: MAX_MDX_CONTENT_WIDTH,
+          }}
+        >
+          {children}
+        </Box>
+      </Center>
+    ),
+  };
 
-        if (href!.startsWith('/')) props.component = Link;
-        else props.target = '_blank';
-
-        return (
-            <Anchor href={href!} {...props}>
-                {children}
-            </Anchor>
-        );
-    };
-
-    const customComponents: MDXComponents = {
-        a: ({ href, children }) => (
-            <DynamicLink href={href}>{children}</DynamicLink>
-        ),
-        blockquote: ({ children }) => (
-            <Blockquote
-                color={'blue'}
-                mt={'md'}
-                mb={'lg'}
-                pl={'lg'}
-            >
-                {children}
-            </Blockquote>
-        ),
-        code: ({ children }) => <InlineCode>{children}</InlineCode>,
-        em: ({ children }) => (
-            <LargeText component={'span'} fs={'italic'}>
-                {children}
-            </LargeText>
-        ),
-        h1: ({ children }) => (
-            <Title
-                order={1}
-                style={{
-                    paddingTop: theme.spacing.md,
-                    paddingBottom: theme.spacing.md,
-                }}
-            >
-                {children}
-            </Title>
-        ),
-        h2: ({ children }) => (
-            <Title
-                order={2}
-                style={{
-                    paddingTop: theme.spacing.xl,
-                    paddingBottom: theme.spacing.sm,
-                    borderBottom: `1px solid light-dark(${theme.colors.gray[2]}, ${theme.colors.dark[4]})`,
-                    marginBottom: theme.spacing.sm,
-                }}
-            >
-                {children}
-            </Title>
-        ),
-        h3: ({ children }) => (
-            <Title
-                order={3}
-                style={{
-                    paddingTop: theme.spacing.lg,
-                    paddingBottom: theme.spacing.xs,
-                }}
-            >
-                {children}
-            </Title>
-        ),
-        h4: ({ children }) => (
-            <Title
-                order={4}
-                style={{
-                    paddingTop: theme.spacing.md,
-                    paddingBottom: theme.spacing.xs,
-                }}
-            >
-                {children}
-            </Title>
-        ),
-        h5: ({ children }) => (
-            <Title
-                order={5}
-                style={{
-                    paddingTop: theme.spacing.sm,
-                    paddingBottom: theme.spacing.xs,
-                }}
-            >
-                {children}
-            </Title>
-        ),
-        h6: ({ children }) => (
-            <Title
-                order={6}
-                style={{
-                    paddingTop: theme.spacing.sm,
-                    paddingBottom: theme.spacing.xs,
-                }}
-            >
-                {children}
-            </Title>
-        ),
-        hr: () => <Divider my={'lg'} />,
-        li: ({ children }) => (
-            <List.Item
-                style={{
-                    paddingBottom:
-                        'calc(var(--mantine-spacing-xs) * 0.3)',
-                }}
-            >
-                {children}
-            </List.Item>
-        ),
-        ol: ({ children }) => (
-            <List mb={'md'} type={'ordered'}>
-                {children}
-            </List>
-        ),
-        p: ({ children }) => (
-            <LargeText pb={'sm'}>{children}</LargeText>
-        ),
-        strong: ({ children }) => (
-            <LargeText component={'span'} fw={700}>
-                {children}
-            </LargeText>
-        ),
-        ul: ({ children }) => (
-            <List mb={'md'} type={'unordered'}>
-                {children}
-            </List>
-        ),
-        wrapper: ({ children }) =>
-            isDocs ? (
-                <Box
-                    style={{
-                        width: '100%',
-                        maxWidth: MAX_MDX_CONTENT_WIDTH,
-                        paddingLeft: theme.spacing.xl,
-                        paddingRight: theme.spacing.xl,
-                        marginBottom:
-                            'calc(var(--mantine-spacing-xl) * 1.25)',
-                        marginTop: theme.spacing.md,
-                    }}
-                >
-                    {children}
-                </Box>
-            ) : (
-                <Center
-                    style={{
-                        marginBottom:
-                            'calc(var(--mantine-spacing-xl) * 1.25)',
-                        marginTop: 'var(--mantine-spacing-xl)',
-                    }}
-                >
-                    <Box
-                        style={{
-                            width: '100%',
-                            maxWidth: MAX_MDX_CONTENT_WIDTH,
-                        }}
-                    >
-                        {children}
-                    </Box>
-                </Center>
-            ),
-    };
-
-    return {
-        ...components,
-        ...customComponents,
-    };
+  return {
+    ...components,
+    ...customComponents,
+  };
 }
