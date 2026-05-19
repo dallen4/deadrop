b72046732fc2f0a3fa6fe425928f2bde442c0cac
Nieky Allen <nieky.allen@gmail.com>
2026-05-18 23:12:24 -0500

    fix: resolve url formatting

--- web/tests/e2e/config.ts
+++ web/tests/e2e/config.ts
@@ -1,7 +1,11 @@
 const PORT = process.env.PORT || 3000;
 
-export const baseURL =
+const rawBaseURL =
   process.env.TEST_URI || `http://localhost:${PORT}/`;
+
+export const baseURL = rawBaseURL.endsWith('/')
+  ? rawBaseURL
+  : `${rawBaseURL}/`;
 
 export const isLocal = baseURL.includes('localhost');
 
