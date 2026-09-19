4ff4787b2ae2017edc110142d2999f0cc2a491b3
Nieky Allen <nieky.allen@gmail.com>
2026-09-19 17:42:18 -0500

    feat(worker): return the current user from the auth me route

--- worker/src/routers/auth/index.ts
+++ worker/src/routers/auth/index.ts
@@ -4,8 +4,32 @@ import { authenticated } from '../../lib/middleware';
 import apiKeysRouter from './apiKeys';
 
 const authRouter = hono()
-  .use(authenticated())
-  .get(AppRouteParts.Me)
+  .use(authenticated({ required: true }))
+  .get(AppRouteParts.Me, async (c) => {
+    const userId = c.get('userId')!;
+
+    const {
+      primaryEmailAddress: email,
+      banned,
+      locked,
+      publicMetadata: metadata,
+      createdAt,
+      lastActiveAt,
+    } = await c.var.clerk.users.getUser(userId);
+
+    return c.json(
+      {
+        id: userId,
+        email,
+        banned,
+        locked,
+        metadata,
+        lastActiveAt: new Date(lastActiveAt!),
+        createdAt: new Date(createdAt),
+      },
+      200,
+    );
+  })
   .get(AppRouteParts.CreateSignInToken, async (c) => {
     const userId = c.get('userId')!;
 
